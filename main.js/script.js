581c4daacf10bbcf31adf762a65f48b1642c5157
Ka-baby <vika25gz@student.ju.se>
2026-05-18 20:43:08 +0200

    Added CSS and JS for map page
    Added all needed elements of the map page and fixed a lot of things to match up with the other pages. Fully redid the CSS as well as added some JS fixes that didn't work with the other pages.

--- main.js/script.js
+++ main.js/script.js
@@ -23,3 +23,65 @@ window.onclick = function (e) {
     modal.style.display = "none";
   }
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  const markers = document.querySelectorAll(".map-marker");
+
+  //source for the math below: https://easings.net/#easeInOutCubic
+  function easeInOutCubic(x) {
+    return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+  }
+  //source: https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+  function smoothScrollTo(target, duration) {
+    const targetPosition = target.getBoundingClientRect().top + window.scrollY;
+    const startPosition = window.scrollY;
+
+    const distance = targetPosition - startPosition - 50;
+    let startTime = null;
+
+    function animation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+
+      const progress = Math.min(timeElapsed / duration, 1);
+
+      const ease = easeInOutCubic(progress);
+
+      window.scrollTo(0, startPosition + distance * ease);
+
+      //source :https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame
+      if (timeElapsed < duration) {
+        requestAnimationFrame(animation);
+      }
+    }
+
+    requestAnimationFrame(animation);
+  }
+
+  markers.forEach((marker) => {
+    marker.addEventListener("click", function () {
+      const targetID = this.getAttribute("data-event") + "-details";
+      const targetElement = document.getElementById(targetID);
+
+      if (targetElement) {
+        smoothScrollTo(targetElement, 1000);
+
+        const title = targetElement.querySelector("h2");
+
+        if (title) {
+          title.classList.remove("highlight-title");
+          //animation reset adapted from: https://css-tricks.com/restart-css-animation/
+          void title.offsetWidth;
+          title.classList.add("highlight-title");
+        }
+        const arrow = targetElement.querySelector(".food-arrow");
+
+        if (arrow) {
+          arrow.classList.remove("highlight-title");
+          void arrow.offsetWidth;
+          arrow.classList.add("highlight-title");
+        }
+      }
+    });
+  });
+});
